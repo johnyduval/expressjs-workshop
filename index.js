6a940a1df9bc3198743111907fe221d3aa779ec9
Johny Junior Duval <johny.duval@gmail.com>
2015-11-30 20:19:23 +0000

    completed exercise 2

--- index.js
+++ index.js
@@ -5,8 +5,39 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/hello/:firstName', function (req,res){
+  var entryId = req.params.firstName;
 
+  res.send('Hello ' + entryId + '!');
+  
+});
 
+/* app.get('/op/:operation/:number1/:number2', function (req,res){
+  
+  var result;
+  
+  if (req.params.operation === 'add'){
+    result = Number(req.params.number1) + Number(req.params.number2);
+  } else if (req.params.operation === 'sub'){
+    result = Number(req.params.number1) - Number(req.params.number2);
+  } else if (req.params.operation === 'mult'){
+    result = Number(req.params.number1) * Number(req.params.number2); 
+  } else if (req.params.operation === 'div'){
+    result = Number(req.params.number1) / Number(req.params.number2);
+  } else {
+    res.sendStatus(403);
+  }
+
+  var obj = {
+    operator: req.params.operation,
+    firstOperand: req.params.number1,
+    secondOperand: req.params.number2,
+    solution: result
+  };
+
+  res.json(obj);
+  
+}); */
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
